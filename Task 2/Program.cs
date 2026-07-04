e9fc5eadf40b730f3d0da6350ef4aa1ef2a3ba9e
Mariyam Talal Al-Hosni <s150340@student.squ.edu.om>
2026-07-04 23:56:30 +0400

    added task 6-10 (completed task 2!)

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -16,7 +16,7 @@ namespace Task_2
             Console.Write("Enter a starting number(int):");
             int number = Convert.ToInt32(Console.ReadLine());
 
-            for(int i = number; i !=0; i--)
+            for (int i = number; i != 0; i--)
             {
                 Console.WriteLine(i);
             }
@@ -34,11 +34,11 @@ namespace Task_2
             int wholeNum = Convert.ToInt32(Console.ReadLine());
 
 
-            for (int i = 1; i<= wholeNum; i++)
+            for (int i = 1; i <= wholeNum; i++)
             {
-                sum+=i;
+                sum += i;
             }
-            Console.WriteLine("Final sum :"+sum);
+            Console.WriteLine("Final sum :" + sum);
 
 
             //Task 3 - Multiplication Table
@@ -54,7 +54,7 @@ namespace Task_2
 
             for (int i = 1; i < 11; i++)
             {
-                Console.WriteLine(i+"x"+ num3+"="+(i* num3));
+                Console.WriteLine(i + "x" + num3 + "=" + (i * num3));
 
             }
 
@@ -70,7 +70,7 @@ namespace Task_2
             {
                 Console.WriteLine("Incorrect password, try again");
                 Console.WriteLine("Enter the password:");
-                 check = Console.ReadLine();
+                check = Console.ReadLine();
 
             }
             Console.WriteLine("Access Granted");
@@ -84,7 +84,7 @@ namespace Task_2
             int counter = 0;
             int secretNum = 42;
             int numGuess = 0;
-            
+
 
             //loop body
             do
@@ -102,7 +102,266 @@ namespace Task_2
             Console.WriteLine("");
             Console.WriteLine("Task 6 - Safe Division Calculator");
             Console.WriteLine("");
+            //ask user to enter 2 numbers
+            try
+            {
 
+                Console.WriteLine("Enter the first number:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the second number:");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine(num1 / num2);
+            }
+
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("We cannot divide by zero");
+
+            }
+            catch (Exception ex) { Console.WriteLine("Input is not a valid number"); }
+
+
+            //Task 7 - Repeating Menu with Exit Option
+            Console.WriteLine("");
+            Console.WriteLine("Task 7 - Repeating Menu with Exit Option");
+            Console.WriteLine("");
+
+            //printing menu 
+            string quit = "";
+
+            while (quit != "Q")
+            {
+                try
+                {
+                    //keep printing menu
+                    Console.WriteLine("-----------------------------");
+                    Console.WriteLine("Please select an option :");
+                    Console.WriteLine("1) Say Hello ");
+                    Console.WriteLine("2) Say Good morning ");
+                    Console.Write("3) Exit ");
+                    int menuChoice = Convert.ToInt32(Console.ReadLine());
+                    //choice
+                    switch (menuChoice)
+                    {
+                        case 1:
+                            Console.WriteLine("Hello");
+                            break;
+                        case 2:
+                            Console.WriteLine("Good morning");
+                            break;
+                        case 3:
+                            quit = "Q";
+                            break;
+                        default:
+                            Console.WriteLine("That number isnt one of the options");
+                            break;
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(" ERROR :Thats not a number");
+                }
+
+
+            }
+
+
+            //Task 8 - Sum of Even Numbers Only
+            Console.WriteLine("");
+            Console.WriteLine("Task 8 - Sum of Even Numbers Only");
+            Console.WriteLine("");
+
+
+            //set varaible sum
+            int sumEven = 0;
+            //user input
+            Console.Write("Enter a positive whole number N:");
+            int wholeNumN = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 1; i <= wholeNumN; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sumEven += i;
+                }
+            }
+            Console.WriteLine("Final sum :" + sumEven);
+
+            //Task 9 - Validated Positive Number Input
+            Console.WriteLine("");
+            Console.WriteLine("Task 9 - Validated Positive Number Input");
+            Console.WriteLine("");
+
+            //set positive number
+            int positiveNum = 0; // only did this so compiler runs my code
+            bool checker = true;
+            do
+            {
+                try
+                {
+                    Console.Write("Enter a positive whole number N:");
+                    positiveNum = Convert.ToInt32(Console.ReadLine());
+                    //checks if its zero or negative
+                    if (positiveNum > 0) //we check here
+                    {
+                        checker = false; //this will break the loop when a positive number is entered
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("ERROR :That's not a valid positve number");
+                }
+            } while (checker);
+
+
+            int amount = 0;
+            //here we will do the printing
+            for (int i = 1; i <= positiveNum; i++)
+            {
+                amount += i;
+            }
+            Console.WriteLine("Final sum :" + amount);
+
+
+
+            //Task 10 - Simple ATM Simulation
+            Console.WriteLine("");
+            Console.WriteLine("Task 10 - Simple ATM Simulation");
+            Console.WriteLine("");
+
+            //set variables
+            int fixedPin = 1234;
+            double balance = 100.0;
+            int attempts = 3;
+            int tries = 0;
+            int pin = 0;
+
+            do
+            {
+                try
+                {
+                    Console.Write("Enter PIN:");
+                    pin = Convert.ToInt32(Console.ReadLine());
+                    tries++;
+                    if (pin != fixedPin)
+                    {
+                        Console.WriteLine("Wrong PIN");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Wrong PIN");
+                    tries++;
+                }
+
+            } while ((tries != attempts) && (pin != fixedPin));
+
+            if (pin != fixedPin)
+            {
+                Console.WriteLine("Card Blocked");
+            }
+            else
+            {
+              
+                string stop = "";
+
+                while (stop != "Q")
+                {
+
+
+                    //keep printing menu
+                    Console.WriteLine("-----------------------------");
+                    Console.WriteLine("Please select an option :");
+                    Console.WriteLine("1) Deposit");
+                    Console.WriteLine("2) Withdraw");
+                    Console.Write("3) Check Balance ");
+                    Console.Write("4) Exit ");
+                    int menuNum = Convert.ToInt32(Console.ReadLine());
+                    //read choices and catch error
+                    try
+                    {
+                        switch (menuNum)
+                        {
+                            case 1:
+                                Console.Write("Enter amount to deposit");
+                                double deposit = Convert.ToDouble(Console.ReadLine());
+                                if (deposit <=0)
+                                {
+                                    Console.Write("Invalid amount to deposit");
+
+                                }
+                                else
+                                {
+                                    balance += deposit; //add to balance
+                                    Console.WriteLine("Your balance is :" + balance);
+                                }
+
+                                break;
+                            case 2:
+                                Console.Write("Enter amount to withdraw");
+                                double withdraw = Convert.ToDouble(Console.ReadLine());
+                                if ((withdraw <= 0) || (withdraw >balance))
+                                {
+                                    Console.Write("Invalid amount to withdraw");
+
+                                }
+                                else
+                                {
+                                    balance-= withdraw; //deduct from balance
+                                    Console.WriteLine("Your balance is :" + balance);
+                                }
+                                break;
+                            case 3:
+                                Console.WriteLine("Your balance is :"+balance);
+                                break;
+                            case 4:
+                                stop = "Q";
+                                break;
+                            default:
+                                Console.WriteLine("That number isnt one of the options");
+                                break;
+
+                        }
+
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine("Invalid input");
+                    }
+
+
+
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
         }
     }
 }
+    
+    
+
+
+
+
