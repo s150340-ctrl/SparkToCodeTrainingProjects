802563026a2b6a0bd4730cc6a73f5a0f411fe40e
Mariyam Talal Al-Hosni <s150340@student.squ.edu.om>
2026-07-05 12:35:39 +0400

    Task 1 - Absolute Difference solved

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-           
+            //Task 1 - Absolute Difference
+            Console.WriteLine();
+            Console.WriteLine("Task 1 - Absolute Difference");
+            Console.WriteLine();
+
+
+
+            //ask user
+            try
+            {
+                Console.Write("Enter the first number:");
+                 int firstNum= Convert.ToInt32((string) Console.ReadLine());
+                Console.Write("Enter the second number:");
+                 int secondNum = Convert.ToInt32((string)Console.ReadLine());
+                //compute difference
+                Console.WriteLine("Absolute difference is:"+(Math.Abs(firstNum - secondNum)));
+            
+
+            }
+            catch (Exception ex)
+            { Console.WriteLine("that's not a valid number"); }
+
         }
     }
 }
+
