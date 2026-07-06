c04f72de88080f4fe8c7ba51793ef90f36c85282
Mariyam Talal Al-Hosni <s150340@student.squ.edu.om>
2026-07-06 16:30:36 +0400

    Task 1 - Personalized Welcome Function

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            
+            //Task 1 - Personalized Welcome Function
+            Console.WriteLine();
+            Console.WriteLine("Task 1 - Personalized Welcome Function");
+            Console.WriteLine();
+            string name = ""; //set name
+
+            //welcome function
+            static void PrintWelcome(string message) {
+
+                Console.Write("Welcome "+ message);
+
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter your name:");
+                    name=Console.ReadLine();
+                    PrintWelcome(name); //function prints name
+                    break; //leave loop
+                }
+                catch (Exception ex)
+                { Console.WriteLine("ERROR: Invalid input"); }
+            }
+
+
         }
     }
 }
+
