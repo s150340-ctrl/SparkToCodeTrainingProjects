4e0dd5f6a2e2b52d08bd425e0d0bb11e91ec1e2d
Mariyam Talal Al-Hosni <s150340@student.squ.edu.om>
2026-07-08 10:39:06 +0400

    Task 1 - Fixed Grades Array

--- Task 5/Program.cs
+++ Task 5/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-           
+            //Task 1 - Fixed Grades Array
+            Console.WriteLine();
+            Console.WriteLine("Task 1 - Fixed Grades Array");
+            Console.WriteLine();
+            //set an array
+            int[] grades = new int[5];
+            //read from user
+            while (true)
+            {
+                try
+                {
+                    for (int i = 0; i < grades.Length; i++)
+                    {
+                        Console.Write("Enter your grades:");
+                        grades[i] =int.Parse(Console.ReadLine());
+                    }
+                    //after we print 
+                    Console.WriteLine("Your grades are:");
+                    foreach (int x in grades)
+                    {
+                        Console.WriteLine(x);
+                    }
+                    //leave loop
+                    break;
+                }
+                catch (Exception e) {
+                    Console.WriteLine("Error invalid input.Try again.");
+
+
+                }
+            }
         }
     }
 }
