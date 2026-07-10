0c92eb228f0de777ee451d631041cbec4003e348
Mariyam Talal Al-Hosni <s150340@student.squ.edu.om>
2026-07-10 14:43:48 +0400

    fixed som issues added option 5-6-7

--- BankingSystemApp/Program.cs
+++ BankingSystemApp/Program.cs
@@ -22,8 +22,8 @@ namespace BankingSystemApp
                 Console.WriteLine("3. Withdraw Money");
                 Console.WriteLine("4. Show Balance");
                 Console.WriteLine("5. Transfer Amount");
-                Console.WriteLine("6. Delete account");
-                Console.WriteLine("7. Apply for membership");
+                Console.WriteLine("6. List all accounts");
+                Console.WriteLine("7. Find the Richest Customer");
                 Console.WriteLine("8. Exit");
                 Console.Write("Choose an option: ");
                 int choice;
@@ -54,10 +54,10 @@ namespace BankingSystemApp
                         TransferAmount();
                         break;
                     case 6:
-                        DeleteAccount();
+                        ListAll();
                         break;
                     case 7:
-                        SearchAccount();
+                        FindRichest();
                         break;
                     case 8:
                         exitApp = true;
@@ -76,7 +76,8 @@ namespace BankingSystemApp
         // for these services - it only shows the menu and calls them.
         static void AddAccount()
         {
-            while (true) {
+            while (true)
+            {
                 try
                 {
                     Console.Write("Enter your name: ");
@@ -85,7 +86,7 @@ namespace BankingSystemApp
                     string number = Console.ReadLine().ToLower();
 
                     double amount = -1;
-                    while(amount< 0)//keeps on assking until user enters a positive amount or non-negative
+                    while (amount < 0)//keeps on assking until user enters a positive amount or non-negative
                     {
                         Console.Write("Enter your initial deposit amount : ");
                         amount = double.Parse(Console.ReadLine());
@@ -98,7 +99,7 @@ namespace BankingSystemApp
                         {
                             if (c == number)
                             {
-                                Console.Write("ERROR : number already exists");
+                                Console.WriteLine("ERROR : number already exists");
                                 check = true;
                                 break; //leave loop
 
@@ -115,9 +116,9 @@ namespace BankingSystemApp
                             accountNumbers.Add(number);
                             balances.Add(amount);
                             Console.WriteLine("Your new bank account details:");
-                            Console.WriteLine("Account name: "+ name);
-                            Console.WriteLine("Account ID :"+ number);
-                            Console.WriteLine("Balance: "+ amount);
+                            Console.WriteLine("Account name: " + name);
+                            Console.WriteLine("Account ID :" + number);
+                            Console.WriteLine("Balance: " + amount);
                             break; //leave outer loop after adding an account
 
                         }
@@ -133,10 +134,11 @@ namespace BankingSystemApp
                         Console.WriteLine("Balance: " + amount);
 
                         break;
-                    } }
+                    }
+                }
                 catch (Exception) { Console.WriteLine("ERROR: invalid input."); }
             }
-          
+
         }
         static void DepositMoney()
         {
@@ -144,7 +146,7 @@ namespace BankingSystemApp
             {
                 try
                 {
-                  
+
                     Console.Write("Enter your account number : ");
                     string number = Console.ReadLine().ToLower();
                     int indexAccount = 0;
@@ -153,7 +155,8 @@ namespace BankingSystemApp
                     {
                         for (int i = 0; i < accountNumbers.Count; i++)
                         {
-                            if (accountNumbers[i] == number) { // number is found
+                            if (accountNumbers[i] == number)
+                            { // number is found
                                 indexAccount = i;
                                 accountCheck = false;
                                 break;
@@ -161,7 +164,7 @@ namespace BankingSystemApp
                         }
                         if (accountCheck)//still true means account DNE
                         {
-                            Console.Write("ERROR : number doesn't exist");
+                            Console.WriteLine("ERROR : number doesn't exist");
                             break; //leave loop
 
                         }
@@ -180,18 +183,27 @@ namespace BankingSystemApp
                             //print updated balance
                             Console.WriteLine("Your updated balance: " + balances[indexAccount]);
                             break;
-                            
+
 
 
                         }
                     }
-                    
-                
+                    else
+                    {
+
+                        Console.WriteLine("ERROR : number doesn't exist.Please add more accounts");
+                        break; //leave loop
+
+
+
+                    }
+
+
                 }
                 catch (Exception) { Console.WriteLine("ERROR: invalid input."); }
             }
 
-        
+
         }
         static void WithdrawMoney()
         {
@@ -217,7 +229,7 @@ namespace BankingSystemApp
                         }
                         if (accountCheck)//still true means account DNE
                         {
-                            Console.Write("ERROR : number doesn't exist");
+                            Console.WriteLine("ERROR : number doesn't exist");
                             break; //leave loop
 
                         }
@@ -225,13 +237,16 @@ namespace BankingSystemApp
                         {
                             //ask for amount
                             double withdraw = -1;
-                            while ((withdraw < 0)&& (withdraw > balances[indexAccount]))//keeps on assking until user enters a positive amount or non-negative
+                            while ((withdraw < 0))//keeps on assking until user enters a positive amount or non-negative
                             {
                                 Console.Write("Enter your withdrawal amount : ");
                                 withdraw = double.Parse(Console.ReadLine());
-                                if((withdraw < 0) && (withdraw > balances[indexAccount]))
+                                if ((withdraw < 0) || (withdraw > balances[indexAccount]))
                                 {
-                                    Console.Write("ERROR : your number is neqative or it exceedes balance amount.");
+                                    withdraw = -1;
+
+                                    Console.WriteLine("ERROR : your number is neqative or it exceedes balance amount.");
+
                                 }
 
                             }
@@ -245,6 +260,14 @@ namespace BankingSystemApp
 
                         }
                     }
+                    else
+                    {
+
+                        Console.WriteLine("ERROR : number doesn't exist.Please add more accounts");
+                        break; //leave loop
+
+
+                    }
 
 
                 }
@@ -253,7 +276,7 @@ namespace BankingSystemApp
 
 
         }
-        
+
         static void ShowBalance()
         {
             while (true)
@@ -278,7 +301,7 @@ namespace BankingSystemApp
                         }
                         if (accountCheck)//still true means account DNE
                         {
-                            Console.Write("ERROR : number doesn't exist");
+                            Console.WriteLine("ERROR : number doesn't exist");
                             break; //leave loop
 
                         }
@@ -296,6 +319,14 @@ namespace BankingSystemApp
 
                         }
                     }
+                    else //means there is nothing in the list
+                    {
+
+                        Console.WriteLine("ERROR : number doesn't exist.Please add more accounts");
+                        break; //leave loop
+
+
+                    }
 
 
                 }
@@ -304,20 +335,145 @@ namespace BankingSystemApp
 
 
         }
-        
+
         static void TransferAmount()
         {
-            // TODO: implement this service (see Section 3 requirements)
-        }
-        static void DeleteAccount()
-        {
+            while (true)
+            {
+                try
+                {
+
+                    Console.Write("Enter the sender's account number : ");
+                    string senderNum = Console.ReadLine().ToLower();
+                    Console.Write("Enter the  receiver's account number : ");
+                    string receiverNum = Console.ReadLine().ToLower();
+                    int indexAccountSend = 0;
+                    int indexAccountRec = 0;
+                    bool accountCheckSend = true;
+                    bool accountCheckRec = true;
+                    if (accountNumbers.Count > 0) // if not empty
+                    {
+                        for (int i = 0; i < accountNumbers.Count; i++)
+                        {
+                            if (accountNumbers[i] == senderNum)
+                            { // number is found
+                                indexAccountSend = i;
+                                accountCheckSend = false;
+                            }
+                            if (accountNumbers[i] == senderNum)
+                            {
+                                indexAccountRec = i;
+                                accountCheckRec = false;
+
+                            }
+                        }
+                        if (accountCheckRec || accountCheckSend)//still true means account DNE
+                        {
+                            Console.WriteLine("ERROR : One or both numbers dont exist");
+                            break; //leave loop
+
+                        }
+                        else
+                        {
+                            //now we transfer amount
+                            double transfer = -1;
+                            while (transfer < 0)//keep Asking while transfer amount is negative
+                            {
+                                Console.Write("Enter your transfer amount : ");
+                                transfer = double.Parse(Console.ReadLine());
+                                if ((transfer < 0) || (transfer > balances[indexAccountSend]))
+                                {
+                                    transfer = -1;
+
+                                    Console.WriteLine("ERROR : your number is neqative or it exceedes balance amount.");
+
+                                }
+
+                            }
+                            //transfer amount
+                            balances[indexAccountSend] -= transfer;
+                            balances[indexAccountRec] += transfer;
+                            //print customer  balance and info
+                            Console.WriteLine("Your bank account details:");
+
+                            Console.WriteLine("Sender's Balance: " + balances[indexAccountSend]);
+                            Console.WriteLine("Receiver's Balance: " + balances[indexAccountRec]);
+                            break;
+
+
+
+                        }
+                    }
+
+
+                }
+                catch (Exception) { Console.WriteLine("ERROR: invalid input."); }
+            }
+
 
         }
-        static void SearchAccount()
+        static void ListAll()
         {
+            Console.WriteLine("----All bank accounts---");
+            if (accountNumbers.Count > 0) // if not empty
+            {
+                for (int i = 0; i < accountNumbers.Count; i++)
+                {
+                    Console.WriteLine((i + 1) + " bank account details:");
+                    Console.WriteLine("Account name: " + customerNames[i]);
+                    Console.WriteLine("Account ID :" + accountNumbers[i]);
+                    Console.WriteLine("Balance: " + balances[i]);
 
+
+                }
+
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("ERROR : number doesn't exist.Please add more accounts");
+
+            }
         }
+            static void FindRichest()
+            {
+            if (accountNumbers.Count > 0) // if not empty
+            {
+                //first set first balance as max
+                int maxIndex = 0;
+                for (int i = 0; i < accountNumbers.Count; i++)
+                {
+                    if (balances[i] > balances[maxIndex])
+                    {
+                        maxIndex = i;//finds max index
+                    }
+
+
+                }
+                //after we print
+                Console.WriteLine("Richest bank account details:");
+                Console.WriteLine("Account name: " + customerNames[maxIndex]);
+                Console.WriteLine("Account ID :" + accountNumbers[maxIndex]);
+                Console.WriteLine("Balance: " + balances[maxIndex]);
+
+
+
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("ERROR : number doesn't exist.Please add more accounts");
+
+            }
+
+            }
+        }
+    }
+
         // TODO: write two more void, no-parameter functions here for
         // your own custom services (option 6 and option 7)
-    }
-}
+    
