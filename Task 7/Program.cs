33f7e85c30227bc3e706c0d581edeeab5df1c4fb
Mariyam Talal Al-Hosni <s150340@student.squ.edu.om>
2026-07-14 22:10:18 +0400

    added class room

--- Task 7/Program.cs
+++ Task 7/Program.cs
@@ -2,6 +2,43 @@
 {
     internal class Program
     {
+        //making the classes 
+        public class Room
+        {
+            public int roomNumber {  get; set; }
+            public string roomType { get; set; }
+            public double pricePerNight { get; set; }
+            public bool isAvailable { get; set; }
+            //instructers
+            public Room()//defasult
+            {
+
+            }
+            public Room(int number, string typeRoom, double priceNight, bool available)
+            {
+                roomNumber = number;
+                roomType = typeRoom;
+                pricePerNight = priceNight;
+                isAvailable = available;
+            }
+
+
+            
+                //methods
+            public void displayRoom()
+            {
+                Console.WriteLine("Room number : " + roomNumber);
+                Console.WriteLine("Room type : " + roomType);
+                Console.WriteLine("Room price per night : " + roomType);
+                Console.WriteLine("Room status (available?) : " + isAvailable);
+
+            }
+            
+
+            
+
+        }
+
         static void Main(string[] args)
         {
            
