13e7d152bbdd7202a2e578ab0a830c91278acba9
Mariyam Talal Al-Hosni <s150340@student.squ.edu.om>
2026-07-18 17:54:36 +0400

    Renamed project + added classes in seperate files

---
+++ Task 7/Guest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_7
+{
+    public class Guest
+    {
+        public string guestId { get; set; }
+        public string guestName { get; set; }
+        public string roomNumber { set; get; } = "Not Assigned";
+        public DateTime checkInDate { set; get; }
+        public int totalNights { set; get; }
+        //constructer
+        public Guest() { }
+        public Guest(string id, string name, string room, DateTime date, int nights)
+        {
+            guestId = id;
+            guestName = name;
+            roomNumber = room;
+            checkInDate = date;
+            totalNights = nights;
+
+        }
+        //methods
+        public void displayGuest()
+        {
+            Console.WriteLine("guest name : " + guestName);
+            Console.WriteLine("guest id : " + guestId);
+            Console.WriteLine("Checked in on : " + checkInDate);
+            Console.WriteLine("Total nights : " + totalNights);
+            Console.WriteLine("Room number : " + roomNumber);
+
+        }
+        public double calculateTotalCost(List<Room> rooms)
+        {
+            if (roomNumber == "Not Assigned") { return 0; }
+            //else we get the room
+            int myRoomNum = int.Parse(roomNumber);
+            Room myRoom = rooms.FirstOrDefault(r => r.roomNumber == myRoomNum);
+            if (myRoom == null) { return 0; }
+            else
+            {
+                return myRoom.pricePerNight * totalNights;
+            }
+
+
+
+
+        }
+
+    }
+}
---
+++ Task 7/Room.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_7
+{
+    public class Room
+    {
+        public int roomNumber { get; set; }
+        public string roomType { get; set; }
+        public double pricePerNight { get; set; }
+        public bool isAvailable { get; set; } = true;
+        //instructers
+        public Room()//defasult
+        {
+
+        }
+        public Room(int number, string typeRoom, double priceNight)
+        {
+            roomNumber = number;
+            roomType = typeRoom;
+            pricePerNight = priceNight;
+
+        }
+
+
+
+        //methods
+        public void displayRoom()
+        {
+            Console.WriteLine("Room number : " + roomNumber);
+            Console.WriteLine("Room type : " + roomType);
+            Console.WriteLine("Room price per night : " + pricePerNight);
+            Console.WriteLine("Room status (available?) : " + isAvailable);
+
+        }
+
+
+    }
+}
