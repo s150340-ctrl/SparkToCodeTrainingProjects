c8ef6ebf7cf1bf4122dd0502784042dfa2c88499
Mariyam Talal Al-Hosni <s150340@student.squ.edu.om>
2026-07-22 21:32:39 +0400

    added the new information to the records

--- SQL tasks/CompanyDB.sql
+++ SQL tasks/CompanyDB.sql
@@ -92,3 +92,56 @@ FOREIGN KEY (Essn)
 REFERENCES Employee(Ssn)
 ON DELETE CASCADE --baiscally delets all info of dependant once employee ssn gets removed
 );
+
+--part 3 CRUD operations
+INSERT INTO department(Dep_no,Name)
+values(3,'Computer');
+INSERT INTO department(Dep_no,Name)
+values(1,'Science');
+INSERT INTO department(Dep_no,Name)
+values(2,'Math');
+INSERT INTO Employee(Ssn,Fname, Lname,Sex, Salary,Dep_no)
+values('11111111111','Sarah','Al-Hosni','f',1000,3);
+INSERT INTO Employee(Ssn,Fname, Lname,Sex, Salary,Dep_no,Supervisor_snn)
+values('12345678910','Reem','Al-Hosni','f',10000.34,2,'11111111111');
+INSERT INTO Employee(Ssn,Fname, Lname,Sex, Salary,Dep_no,Supervisor_snn)
+values('22222211111','Mariyam','Al-Hosni','f',2345,2,'11111111111');
+INSERT INTO Employee(Ssn,Fname,Minit, Lname,Sex, Salary,Dep_no,Supervisor_snn)
+values('22222222222','Abdullah','T','Al-Hosni','m',2.5,1,'12345678910');
+INSERT INTO department(Dep_no,Name,Manager_ssn,manager_start_date)
+values(4,'English','11111111111', '01-11-2020');
+INSERT INTO Employees_Dependent (Essn, Dname, Sex, Bdate, Relationship)
+VALUES ('11111111111', 'Tommy', 'M', '2015-08-10', 'Son');
+INSERT INTO Employees_Dependent (Essn, Dname, Sex, Bdate, Relationship)
+values('22222222222' , 'Ahmed','m', '11-11-2011','Brother');
+
+--add project
+INSERT INTO Project(Pname, dep_no, Plocation, Pnumber)
+Values('Adding 1+1', 2, 'China',300);
+
+
+--add someone to work on project
+INSERT INTO Works_on(E_ssn, Project_no, PHours)
+values('12345678910',300,200)
+INSERT INTO Works_on(E_ssn, Project_no, PHours)
+values('22222211111',300,24)
+--give emplyee reem a raise for working for 200 hours
+UPDATE Employee SET Salary = Salary + 999999.1 WHERE Ssn='12345678910';
+UPDATE  Employee SET Employee_Address ='MUSCAT' WHERE Sex='f';
+--change employee to another dep
+UPDATE Employee SET Dep_no=4 WHERE Fname='Abdullah';
+UPDATE Employees_Dependent SET Relationship='co-worker' WHERE Essn='11111111111';
+UPDATE Project SET Plocation='America';
+UPDATE Works_on SET PHours =5;
+
+
+--DELETE abdullah to see what happens to his dependent
+DELETE FROM Employee WHERE Fname='Abdullah';
+--delete mariyam to see what happens to her work
+DELETE FROM Works_on WHERE E_ssn='22222211111';
+--then we delete her safley from the employees
+DELETE FROM Employee WHERE Ssn='22222211111';
+-- i shouldved added delete on cascade to the ssn on work_on for automatic clean up 
+--at least i know the diffrences now between deleting immediatly the whol data and manually removing it step by step so no error shows
+
+
