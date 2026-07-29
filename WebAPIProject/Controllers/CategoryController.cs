178512dcfc396a85130d4949315ce193ad3c7aae
Mariyam Talal Al-Hosni <s150340@student.squ.edu.om>
2026-07-29 23:51:20 +0400

    save all changes made

---
+++ WebAPIProject/Controllers/CategoryController.cs
@@ -0,0 +1,46 @@
+﻿using WebAPIProject.Models;
+
+namespace WebAPIProject.Controllers
+{
+    public class CategoryController
+    {
+        private ProjectContext context;
+
+        CategoryController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
+        public void AddCategory(Category c)
+        {
+            context.categories.Add(c);
+            context.SaveChanges();
+        }
+        public void RemoveCategory(int id)
+        {
+            var category = context.categories.FirstOrDefault(c => c.CategoryId == id);
+            if (category != null)
+            {
+                context.categories.Remove(category);
+                context.SaveChanges();
+            }
+            else { }
+        }
+
+
+        public Category GetCategory(int id)
+        {
+            var category = context.categories.FirstOrDefault(c => c.CategoryId == id);
+            return category;
+        }
+        public List<Category> GetAllCategories()
+        {
+            return context.categories.ToList();
+        }
+        public List<Category> GetByName(string name)
+        {
+            return context.categories.Where(c => c.CategoryName.Contains(name)).ToList();
+        }
+
+    }
+}
---
+++ WebAPIProject/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using WebAPIProject.Models;
+
+namespace WebAPIProject.Controllers
+{
+    public class ProductController
+    {
+        private ProjectContext context;
+
+        ProductController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
+        public void AddProduct(Product p)
+        {
+            context.products.Add(p);
+            context.SaveChanges();
+        }
+        public void RemoveProduct(int id)
+        {
+            var product = context.products.FirstOrDefault(p => p.ProductId == id);
+            if (product != null)
+            {
+                context.products.Remove(product);
+                context.SaveChanges();
+            }
+            else { }
+        }
+        public void UpdateProductPrice(int id, double price)
+        {
+            var product = context.products.FirstOrDefault(p => p.ProductId == id);
+            if (product != null)
+            {
+                product.ProductPrice = price;
+                context.SaveChanges();
+            }
+        }
+        public void UpdateProductName(int id, string newName)
+        {
+            var product = context.products.FirstOrDefault(p => p.ProductId == id);
+            if (product != null)
+            {
+                product.ProductName = newName;
+                context.SaveChanges();
+            }
+        }
+        public Product GetProduct(int id)
+        {
+            var product = context.products.FirstOrDefault(p => p.ProductId == id);
+            return product;
+        }
+        public List<Product> GetAllProducts()
+        {
+            return context.products.ToList();
+        }
+        public List<Product> GetByName(string name)
+        {
+            return context.products.Where(p => p.ProductName.Contains(name)).ToList();
+        }
+    }
+}
